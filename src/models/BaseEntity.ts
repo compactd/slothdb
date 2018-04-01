41c2f33016a6878d402545721dc014d6474b173a
vinz243 <vinz243@gmail.com>
2018-04-01 04:27:28 +0200

    feat(BaseEntity): remove useless find function

--- src/models/BaseEntity.ts
+++ src/models/BaseEntity.ts
@@ -6,6 +6,4 @@
 export default class BaseEntity<S> {
   // tslint:disable-next-line:no-empty
   constructor(idOrProps: Partial<S> | string) {}
-  // tslint:disable-next-line:no-empty
-  find() {}
 }
