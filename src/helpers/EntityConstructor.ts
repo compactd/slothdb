4ce95becc4cb1773bdd897fe1db3ef34675b2cf4
vinz243 <vinz243@gmail.com>
2018-04-02 15:47:37 +0200

    docs(EntityConstructor): make interface private

--- src/helpers/EntityConstructor.ts
+++ src/helpers/EntityConstructor.ts
@@ -2,6 +2,9 @@ import BaseEntity from '../models/BaseEntity'
 import PouchFactory from '../models/PouchFactory'
 import StaticData from '../models/StaticData'
 
+/**
+ * @private
+ */
 export default interface EntityConstructor<S, T extends BaseEntity<S>> {
   desc?: StaticData
   new (factory: PouchFactory<S>, idOrProps: Partial<S> | string): T
